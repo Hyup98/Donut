3332e055969e7d84a50b66f2f387d65c5f2594bb
Hyup <kkddong98@gmail.com>
2021-12-05 13:10:37 +0900

    no message

--- Donut/Donut/소스.cpp
+++ Donut/Donut/소스.cpp
@@ -15,11 +15,11 @@ void gotoxy(int x, int y)
 
 int main(void)
 {
-	const double PI = 3.1415926;
+	const float PI = 3.1415926;
 
 	char const pix[12] = { '.', ',', '-', '~', ':', ';', '=', '!', '*', '#', '$', '@' };
-	const int distanceToScreen = 5;
-	const int distanceToObject = 10;
+	const int distanceToScreen = 10;
+	const int distanceToObject = 20;
 	const int radiusToAxis = 20;
 	const int radius = 5;
 	int size = 0;
@@ -36,38 +36,36 @@ int main(void)
 	float temY;
 	float y;
 	float z;
-	float zBuffer[500][500];
+	float zBuffer[100][100];
 	for (size_t i = 0; i < 100; i++)
 	{
 		for (size_t j = 0; j < 100; j++)
 		{
-			zBuffer[i][j] = 0;
+			zBuffer[i][j] = FLT_MAX;
 		}
 	}
-
 	int index;
 	float L;
 	float tem;
 
-	Point points[(int)(4 * 3.1415926 * radiusToAxis * radius + 1)];
 	while (true)
 	{
-		for (float j = 0; j < 2 * PI; j += 0.02)
+		for (float j = 0; j < 1 * PI; j += 0.02)
 		{
 			for (float i = 0; i < 2 * PI; i += 0.02)//y축 회전
 			{
-				for (double k = 0; k < 2 * PI; k += 0.02) //원 만들기
+				for (float k = 0; k < 2 * PI; k += 0.03) //원 만들기
 				{
 					x = (radiusToAxis + radius * cos(k)) * (cos(j) * cos(i) + sin(j) * sin(j) * sin(i)) - radius * cos(j) * sin(j) * sin(k);
 					y = (radiusToAxis + radius * cos(k)) * (cos(i) * sin(j) - cos(j) * sin(j) * sin(i)) + radius * cos(j) * cos(j) * sin(k);
 					z = cos(j) * (radiusToAxis + radius * cos(k)) * sin(i) + radius * sin(j) * sin(k);
 					L = cos(i) * cos(k) * sin(j) - cos(j) * cos(k) * sin(i) - sin(j) * sin(k) + cos(j) * (cos(j) * sin(k) - cos(k) * sin(j) * sin(i));
-					tem = 1 / z;
+					tem = z;
 					temX = x * distanceToScreen / distanceToObject;
 					temY = y * distanceToScreen / distanceToObject;
-					if (zBuffer[(int)temX + 100][(int)temY + 100] < tem)
+					if (zBuffer[(int)temX + 20][(int)temY + 20] >= tem)
 					{
-						zBuffer[(int)temX + 100][(int)temY + 100] = tem;
+						zBuffer[(int)temX + 20][(int)temY + 20] = tem;
 						index = (int)(L * 8);
 						if (index < 0) {
 							index = 0;
@@ -76,23 +74,28 @@ int main(void)
 						{
 							index = 11;
 						}
-						gotoxy(temX + 50, temY + 50);
+						gotoxy(temX + 100, temY + 100);
 						printf("%c", pix[index]);
 					}
 				}
 			}
-			for (size_t i = 0; i < 500; i++)
+			for (size_t i = 0; i < 100; i++)
 			{
-				for (size_t j = 0; j < 500; j++)
+				for (size_t j = 0; j < 100; j++)
 				{
 					zBuffer[i][j] = 0;
 				}
 			}
-			Sleep(100);
+			Sleep(5);
 			std::system("cls");
+			for (size_t i = 0; i < 100; i++)
+			{
+				for (size_t j = 0; j < 100; j++)
+				{
+					zBuffer[i][j] = FLT_MAX;
+				}
+			}
 		}
-
 	}
 	return 0;
 }
-
